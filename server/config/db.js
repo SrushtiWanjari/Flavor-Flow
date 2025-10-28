375e4c20cb4614fcfe9851dab72817fb9603aad3
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-28 19:16:07 +0530

    changes added

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,11 @@
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
-// export const connectDB = async (mongoURI) => {
-//   try {
-//     await mongoose.connect(mongoURI)
-//     console.log('MongoDB connected')
-//   } catch (err) {
-//     console.error('MongoDB connection error:', err.message)
-//     process.exit(1)
-//   }
-// }
-
-import mongoose from 'mongoose';
-
-export const connectDB = async (url) => {
+export const connectDB = async (mongoURI) => {
   try {
-    if (!url) {
-      console.error('❌ MongoDB URL missing!');
-      process.exit(1);
-    }
-    await mongoose.connect(url);
-    console.log('✅ MongoDB Connected');
+    await mongoose.connect(mongoURI)
+    console.log('MongoDB connected')
   } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    console.error('MongoDB connection error:', err.message)
+    process.exit(1)
   }
-};
-
+}
