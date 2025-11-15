086e65b79b4091cdcd62ef45561a42114c2166dd
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-11-15 11:19:06 +0530

    changes added

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://flavor-flow-api-i407.onrender.com'
 })
 
 export const setToken = token => {
